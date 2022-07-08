5072fdd6b82255c9d07fae9c32b828f5c5581a47
robin8237 <robinlai06@hotmail.com>
2022-07-08 15:41:17 +0800

    commit message goes here

---
+++ web/bug_04.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="filler">
+    <title>Activity 4 Errors</title>
+</head>
+<body>
+    Act 4 Error
+    <!-- Error : Image elements do not have [alt] attributes -->
+    <img src="img/wallpaper-100w.jpg" width="100" height="100" />
+</body>
+</html>
