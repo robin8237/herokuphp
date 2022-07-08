ce6f3f06d24a6b415d41c29277c01c404eab11c2
robin8237 <robinlai06@hotmail.com>
2022-07-08 15:48:36 +0800

    commit message goes here

--- web/bug_04.php
+++ web/bug_04.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
         <!--[user-scalable="no"] is used in the <meta name="viewport"> element or the [maximum-scale] attribute is less than 5. -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
     <meta name="description" content="filler">
     <title>Activity 4 Fixes</title>
 </head>
