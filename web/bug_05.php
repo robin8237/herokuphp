6a487ba77dbaf2cd1bb325680aa5d1e27185406a
robin8237 <robinlai06@hotmail.com>
2022-07-08 16:06:24 +0800

    commit message goes here

--- web/bug_05.php
+++ web/bug_05.php
@@ -4,12 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="filler">
     <title>Activity 4 Errors</title>
-    <style>
-        #id{
-            width: 100px;
-            height: 100px;
-        }
-    </style>
+    <script src="js/Rx.js"></script>
 </head>
 <body>
     
