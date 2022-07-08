1ee70b552926e427cca1ae0e862c5aa6f2d44b0a
robin8237 <robinlai06@hotmail.com>
2022-07-08 16:01:30 +0800

    commit message goes here

--- web/bug_05.php
+++ web/bug_05.php
@@ -4,24 +4,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="filler">
     <title>Activity 4 Errors</title>
+    <style>
+        #id{
+            width: 100px;
+            height: 100px;
+        }
+    </style>
 </head>
 <body>
+    
     Act 4 Error <br>
     <!-- Error : Image elements do not have [alt] attributes -->
-    <img src="img/wallpaper-100w.jpg" width="100" height="100" /> <br>
-
-    <img src="img/wallpaper-100w.jpg" width="100" height="100" /> <br>
-
-    <img src="img/wallpaper-100w.jpg" width="100" height="100" /> <br>
-
-    <img src="img/wallpaper-100w.jpg" width="100" height="100" /> <br>
-
-    <img src="img/wallpaper-100w.jpg" width="100" height="100" /> <br>
-
-    <img src="img/wallpaper-100w.jpg" width="100" height="100" /> <br>
-
-    <img src="img/wallpaper-100w.jpg" width="100" height="100" /> <br>
-
-    <img src="img/wallpaper-100w.jpg" width="100" height="100" /> <br>
+    
 </body>
 </html>
