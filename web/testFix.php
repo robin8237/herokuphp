d8727fe7e22ab7cb7a328427e7edcce7c403ac25
robin8237 <robinlai06@hotmail.com>
2022-07-08 12:57:02 +0800

    commit message goes here

--- web/testFix.php
+++ web/testFix.php
@@ -2,6 +2,8 @@
 <head>
     <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- fix : Document doesn't have a <title> element -->
+    <title>Test Fix</title>
 </head>
 <body>
   Text Fix!
