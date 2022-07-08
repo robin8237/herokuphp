4fb890f775bd4717c4712cca02ac5025e87644ac
robin8237 <robinlai06@hotmail.com>
2022-07-08 15:46:41 +0800

    commit message goes here

--- web/bug_04.php
+++ web/bug_04.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+        <!--[user-scalable="no"] is used in the <meta name="viewport"> element or the [maximum-scale] attribute is less than 5. -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="filler">
-    <title>Activity 4 Errors</title>
+    <title>Activity 4 Fixes</title>
 </head>
 <body>
-    Act 4 Error
-    <!-- Error : Image elements do not have [alt] attributes -->
-    <img src="img/wallpaper-100w.jpg" width="100" height="100" />
+    <p>Act 4 Fix</p>
 </body>
 </html>
