d6126332a934fd9e1bf220e11f71f61dd45534b6
robin8237 <robinlai06@hotmail.com>
2022-07-08 12:51:09 +0800

    commit message goes here

--- web/test.php
+++ web/test.php
@@ -1,7 +1,4 @@
-<!-- fix : <html> element does not have a [lang] attribute -->
-<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   Hello World!
---
+++ web/testFix.php
@@ -0,0 +1,9 @@
+<html lang="en">
+<head>
+    <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  Hello World!
+</body>
+</html>
